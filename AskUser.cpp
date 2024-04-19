b8af567dc5534f3f73490542b61ecc689f8c4e9e
Binson Ephraim <Bed-com-21-21@unima.ac.mw>
2024-04-19 13:06:04 +0200

    first commit

---
+++ AskUser.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+   std:: string name;
+  std::  int age ;
+  std::  cout<< " what is your name";
+   std:: cin >> name;
+   std:: cout<< "what is your age";
+    std::cin>> age;
+   cout << " hello " << name <<" your age is  "<< age;
+    return 0;
+
+}
